88f644632da34d771232527a4a64468647518515
Fabiano Fernandes <fabiano.fernandes@crediclass.com.br>
2019-07-18 10:18:30 -0300

    Módulo Administração - cadastro de usuários e permissões

--- src/main/java/br/com/crediclass/console/service/SendGridMailService.java
+++ src/main/java/br/com/crediclass/console/service/SendGridMailService.java
@@ -5,12 +5,6 @@
  */
 package br.com.crediclass.console.service;
 
-import com.sendgrid.*;
-import com.sendgrid.helpers.mail.Mail;
-import com.sendgrid.helpers.mail.objects.Content;
-import com.sendgrid.helpers.mail.objects.Email;
-import java.io.IOException;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 /**
@@ -20,38 +14,38 @@ import org.springframework.stereotype.Service;
 @Service
 public class SendGridMailService {
 
-    SendGrid sendGrid;
-
-    public SendGridMailService(SendGrid sendGrid) {
-        this.sendGrid = sendGrid;
-    }
-
-    @Value("${templateId}")
-    private String EMAIL_TEMPLATE_ID;
+//    SendGrid sendGrid;
+//
+//    public SendGridMailService(SendGrid sendGrid) {
+//        this.sendGrid = sendGrid;
+//    }
+//
+//    @Value("${templateId}")
+//    private String EMAIL_TEMPLATE_ID;
 
     public void sendMail() {
-        Email from = new Email("naoresponder@crediclass.com.br");
-        String subject = "Sending with SendGrid is Fun";
-       // Email to = new Email("contato@fabianofernandes.adm.br");
-        Email to = new Email("tavares@crediclass.com.br");
-        Content content = new Content("text/html", "Hello, <strong>how are you doing?</strong>");
-        Mail mail = new Mail(from, subject, to, content);
-        //mail.personalization.get(0).addSubstitution("-cliente-", "Example User");
-       //mail.personalization.get(0).addSubstitution("-documento-", "Example User");
-        // mail.personalization.get(0).addSubstitution("-cliente-", "Fabiano Fernandes");
-        mail.setTemplateId(EMAIL_TEMPLATE_ID);
-
-        Request request = new Request();
-        try {
-            request.setMethod(Method.POST);
-            request.setEndpoint("mail/send");
-            request.setBody(mail.build());
-            Response response = this.sendGrid.api(request);
-            sendGrid.api(request);
-
-            // ...
-        } catch (IOException ex) {
-            // ...
-        }
+//        Email from = new Email("naoresponder@crediclass.com.br");
+//        String subject = "Sending with SendGrid is Fun";
+//       // Email to = new Email("contato@fabianofernandes.adm.br");
+//        Email to = new Email("tavares@crediclass.com.br");
+//        Content content = new Content("text/html", "Hello, <strong>how are you doing?</strong>");
+//        Mail mail = new Mail(from, subject, to, content);
+//        //mail.personalization.get(0).addSubstitution("-cliente-", "Example User");
+//       //mail.personalization.get(0).addSubstitution("-documento-", "Example User");
+//        // mail.personalization.get(0).addSubstitution("-cliente-", "Fabiano Fernandes");
+//        mail.setTemplateId(EMAIL_TEMPLATE_ID);
+//
+//        Request request = new Request();
+//        try {
+//            request.setMethod(Method.POST);
+//            request.setEndpoint("mail/send");
+//            request.setBody(mail.build());
+//            Response response = this.sendGrid.api(request);
+//            sendGrid.api(request);
+//
+//            // ...
+//        } catch (IOException ex) {
+//            // ...
+//        }
     }
 }
